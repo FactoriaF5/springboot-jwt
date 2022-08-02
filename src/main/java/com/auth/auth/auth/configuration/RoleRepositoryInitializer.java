c13829c65fc0861798712ef91fb0cfb8f12062bc
Sergi Virgili <svirgilif@gmail.com>
2022-08-02 13:18:59 +0200

    Add An Admin User faker when init the application

--- src/main/java/com/auth/auth/auth/configuration/RoleRepositoryInitializer.java
+++ src/main/java/com/auth/auth/auth/configuration/RoleRepositoryInitializer.java
@@ -2,23 +2,30 @@ package com.auth.auth.auth.configuration;
 ;
 
 import com.auth.auth.models.Role;
+import com.auth.auth.models.User;
 import com.auth.auth.repositories.RoleRepository;
 
+import com.auth.auth.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class RoleRepositoryInitializer {
 
     private RoleRepository roleRepository;
+    private UserRepository userRepository;
+    private PasswordEncoder encoder;
 
-    @Autowired
-    public RoleRepositoryInitializer(RoleRepository roleRepository) {
+    public RoleRepositoryInitializer(RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder encoder) {
         this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+        this.encoder = encoder;
     }
 
     @PostConstruct
@@ -34,5 +41,17 @@ public class RoleRepositoryInitializer {
         );
 
         roleRepository.saveAll(roles);
+
+        if (!userRepository.findAll().isEmpty()) {
+            return;
+        }
+
+        var user = new User();
+        user.setRoles(roleRepository.findAll().stream().collect(Collectors.toSet()));
+        user.setEmail("admin@admin.com");
+        user.setUsername("Admin");
+        user.setPassword(encoder.encode("12345678"));
+
+        userRepository.save(user);
     }
 }
